5fb3e82855617ed9720ee64aec06f32abfc0e982
Deniz UZUN <89178559+ultracold-atom@users.noreply.github.com>
2022-09-02 18:24:53 +0300

    Add files via upload

---
+++ pages/hosts/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <main>
+    <div class="cards">
+
+        <div class="form-section">
+          <h1 class="form-heading">Earn money as an Airbnb host</h1>
+          <h2 class="form-subheading">Tell us a little about your place</h2>
+          <input v-model="title" class="location" placeholder="Title"/>
+          <select  v-model="category" class="location" name="Category" >
+            <option value="" disabled selected>Category</option>
+            <option  v-for="category in categories" :key="category"  :value="category">{{category}}</option>
+          </select>
+          <input v-model="owner" class="location" placeholder="Owner Name"/>
+          <textarea v-model="address" class="location" id="address" placeholder="Address"/>            
+          <input v-model="country" class="location" placeholder="Country" />  
+          <input v-model="roomNumber" class="location" type="number" placeholder="Room Number"/>
+          <input v-model="price" class="location" type="number" placeholder="Price"/>          
+        </div>
+
+        <div class="form-section">  
+          <h2>Features</h2>
+          <div v-for="(feature,index) in features" :key="index"  >
+            <input :id="index" class="toggle" v-model="checkedFeatures" type="checkbox" :value="feature"> 
+            <label :for="index" class="feature-options"> {{feature}} </label>    
+          </div>
+  
+          <h2 class="photo-title">Host Photo</h2>
+          <label for="file-upload" class="custom-file-upload">
+            Upload Your Host Photo
+          </label>
+          <input id="file-upload" type="file" @change="onFileSelected"/>
+
+          <button class="upload-host" @click="onUploadHost">Become a Host</button>
+
+        </div>
+
+
+    </div>     
+
+  </main>  
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      categories: ["House","Bungalov","TreeHouses","Island","North Pole","Cave","Castle","Estate","National Park","Tower","Tropical","Landscape"],
+      features: ["Wifi Connection","Jakuzi","Pool","Camelia"],
+      checkedFeatures: [],
+      title: "",
+      category: "",
+      owner: "",
+      roomNumber: 0,
+      price: 0,
+      address: "",
+      country: "",
+      selectedFile: null,
+      fileName: ""
+    }
+  },
+
+  methods: {
+    onFileSelected(e) {
+      this.selectedFile = e.target.files[0];
+      console.log(this.selectedFile);
+      this.fileName = e.target.files[0].name;
+    },
+    async onUploadHost() {
+      let data = new FormData();
+      data.append("title", this.title);
+      data.append("category", this.category);
+      data.append("owner", this.owner);
+      data.append("address", this.address);      
+      data.append("country", this.country);  
+      data.append("roomNumber", this.roomNumber);   
+      data.append("features", this.checkedFeatures); 
+      data.append("photo", this.selectedFile, this.selectedFile.name);
+      data.append("price", this.price);
+
+      let result = await this.$axios.$post(
+        "http://localhost:3000/api/hosts",
+        data
+      );
+      this.$router.push("/");
+    }
+  }
+
+}
+</script>
+
+
+<style >
+
+  @import '../../static/css/host.css';
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+  name: 'IndexPage',
+  components:{
+    Navbar
+  }
+}
+</script>
